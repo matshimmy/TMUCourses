08b2e1c0c707ea43f612e81f35afef5d1e694b07
Mathew Szymanowski <m96szy@gmail.com>
2023-09-18 15:39:46 -0400

    implemented the function compliment

--- BME506/Lab1Proj/Lab1.cpp
+++ BME506/Lab1Proj/Lab1.cpp
@@ -9,9 +9,9 @@ string enterDNAString() {
 }
 // implement the function checkValidity
 bool checkValidity(string s) {
-  for (int x = 0;x < s.length();x++) {
-    if (s[x] == 'A' || s[x] == 'G' || s[x] == 'T' || s[x] == 'C'
-      || s[x] == 'a' || s[x] == 'g' || s[x] == 't' || s[x] == 'c') {
+  for (int x = 0; x < s.length(); x++) {
+    if (s[x] == 'A' || s[x] == 'G' || s[x] == 'T' || s[x] == 'C' ||
+        s[x] == 'a' || s[x] == 'g' || s[x] == 't' || s[x] == 'c') {
     } else {
       return false;
     }
@@ -19,6 +19,28 @@ bool checkValidity(string s) {
   return true;
 }
 // implement the function compliment
+void compliment(string s) {
+  cout << " : compliment(AGTC) = ";
+  for (int x = 0; x < s.length(); x++) {
+    if (s[x] == 'A') {
+      cout << "T";
+    } else if (s[x] == 'a') {
+      cout << "t";
+    } else if (s[x] == 'G') {
+      cout << "C";
+    } else if (s[x] == 'g') {
+      cout << "c";
+    } else if (s[x] == 'T') {
+      cout << "A";
+    } else if (s[x] == 't') {
+      cout << "a";
+    } else if (s[x] == 'C') {
+      cout << "G";
+    } else if (s[x] == 'c') {
+      cout << "g";
+    }
+  }
+}
 
 int main() {
 
@@ -58,19 +80,17 @@ int main() {
       else
         cout << " : INVALID DNA";
       cout << endl;
-    }
-    //   else if (3 == option) {
-    //   string str1 = enterDNAString();
-    //   cout << str1 << " (" << str1.size() << " chars)";
-    //   bool b = checkValidity(str1);
-    //   if (b == true)
-    //     cout << " : VALID DNA";
-    //   else
-    //     cout << " : INVALID DNA";
-    //   compliment(str1);
-    //   cout << endl;
-    // } 
-    else if (4 == option) {
+    } else if (3 == option) {
+      string str1 = enterDNAString();
+      cout << str1 << " (" << str1.size() << " chars)";
+      bool b = checkValidity(str1);
+      if (b == true) {
+        cout << " : VALID DNA";
+        compliment(str1);
+      } else
+        cout << " : INVALID DNA";
+      cout << endl;
+    } else if (4 == option) {
       return 0;
     } else {
       cout << "Integer option, but incorrect! Try again." << endl;
