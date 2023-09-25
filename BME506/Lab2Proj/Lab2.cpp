2f9808389b02dd7fd6d2873f060f48d0b38b80f9
Mathew Szymanowski <m96szy@gmail.com>
2023-09-25 13:41:33 -0400

    Implemented part 3

--- BME506/Lab2Proj/Lab2.cpp
+++ BME506/Lab2Proj/Lab2.cpp
@@ -21,7 +21,27 @@ void display(const double array[]) {
   }
 }
 // implement the function twoDdisplay
-void twoDdisplay(int num, const double arr[][11]) {}
+void twoDdisplay(int num, const double arr[][11]) {
+  cout << "\n\tAgar Summary:\n\n\tHour";
+  for (int x = 0; x < num; x++) {
+    cout << "\tPopulation (" << x << ")";
+  }
+  cout << "\n\t========";
+  for (int x = 0; x < num; x++) {
+    cout << "==============";
+    if (x != num - 1) {
+      cout << "==";
+    }
+  }
+  cout << endl;
+  for (int x = 0; x < 11; x++) {
+    cout << fixed << setprecision(3) << "\t" << x << "\t";
+    for (int y = 0; y < num; y++) {
+      cout << arr[y][x] << "\t\t\t";
+    }
+    cout << endl;
+  }
+}
 
 int main(int argc, char *argv[]) {
   double k;
@@ -37,6 +57,17 @@ int main(int argc, char *argv[]) {
     display(array);
   } else if (2 == argc) { // Part III
     // WRITE THE CODE FOR Part III HERE.
+    const int num = stoi(argv[1]);
+    double arr[num][11];
+    for (int x = 0; x < num; x++) {
+      cout << "Initializing Bacteria:\n\tGrowth factor (k) [0.0-1.0] : ";
+      cin >> k;
+      cout << "\tInitial population (N0) [0-1000] : ";
+      cin >> n0;
+      cout << endl;
+      calculate(k, n0, arr[x]);
+    }
+    twoDdisplay(num, arr);
   } else if (3 == argc) { // Part II
     // WRITE THE CODE FOR Part II HERE.
     cout << "Initializing Bacteria:\n\tGrowth factor (k) [0.0-1.0] : "
