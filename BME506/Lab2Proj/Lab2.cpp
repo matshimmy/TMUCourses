43e49260424c3cadb41f1a38e6ce6080f2ca9f7a
Mathew Szymanowski <m96szy@gmail.com>
2023-09-25 08:37:34 -0400

    Initialized lab2

---
+++ BME506/Lab2Proj/Lab2.cpp
@@ -0,0 +1,27 @@
+
+#include "Lab2.h"
+#include <cmath>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+
+// implement the function initialize
+// implement the function calculate
+// implement the function display
+
+// implement the function twoDdisplay
+int main(int argc, char *argv[]) {
+  double k;
+  int n0;
+  if (1 == argc) { // Part I
+    // WRITE THE CODE FOR Part I HERE.
+  } else if (2 == argc) { // Part III
+    // WRITE THE CODE FOR Part III HERE.
+  } else if (3 == argc) { // Part II
+    // WRITE THE CODE FOR Part II HERE.
+  } else {
+    cout << "Number of command-line arguments "
+         << "(including the name of the program) "
+         << "should be three or less." << endl;
+  }
+} // end of main
---
+++ BME506/Lab2Proj/Lab2.h
@@ -0,0 +1,12 @@
+#ifndef LAB2_H_
+#define LAB2_H_
+#include <iostream>
+
+using namespace std;
+
+void initialize(double& k, int& n0);
+void calculate( const double k, const int n0, double array[]);
+void display(const double array[]);
+void twoDdisplay(int num, const double arr[][11]);
+
+#endif /* LAB2_H_ */
