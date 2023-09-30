0ba119c40e623c515bc768cf8d3b60a7e459e1d9
Mathew Szymanowski <m96szy@gmail.com>
2023-09-30 09:19:06 -0400

    init lab3

---
+++ BME506/Lab3Proj/Lab3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include "Lab3.h"
+
+// implement the function accessSalaryArrayByPointerArithmetic
+// implement the function sortIntegersByPointerArithmetic
+
+int main(int argc, char *argv[])
+{
+
+    string p1("part1");
+    string p2("part2");
+
+    if (0 == p1.compare(argv[1]))
+    { // PART I
+        // WRITE THE CODE FOR Part I HERE.
+    }
+
+    if (0 == p2.compare(argv[1]))
+    { // PART II
+        // WRITE THE CODE FOR Part I HERE.
+    }
+
+} // end of main
---
+++ BME506/Lab3Proj/Lab3.h
@@ -0,0 +1,10 @@
+#ifndef LAB3_H_
+#define LAB3_H_
+#include <iostream>
+
+using namespace std;
+
+void accessSalaryArrayByPointerArithmetic();
+int *sortIntegersByPointerArithmetic(int *arr, int size);
+
+#endif /* LAB3_H_ */
