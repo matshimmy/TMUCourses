2c721b20c37ca77643023a5c9a91a7cbb08fc689
Mathew Szymanowski <m96szy@gmail.com>
2023-09-30 09:40:05 -0400

    implemented Part 1

--- BME506/Lab3Proj/Lab3.cpp
+++ BME506/Lab3Proj/Lab3.cpp
@@ -1,24 +1,45 @@
-#include <iostream>
-#include <cstdlib>
 #include "Lab3.h"
+#include <cstdlib>
+#include <iostream>
 
 // implement the function accessSalaryArrayByPointerArithmetic
+void accessSalaryArrayByPointerArithmetic() {
+
+  int salary[3];
+  int *psal = salary;
+  
+  for (int i = 0; i < 3; ++i) {
+    cout << "Enter Salary: ";
+    cin >> *(psal + i);
+  }
+  
+  cout << endl;
+  cout << "Entered salaries are: " << endl;
+  for (int i = 0; i < 3; ++i) {
+    cout << *(psal + i) << " ";
+  }
+  
+  cout << endl;
+  cout << "Updated salaries are: " << endl;
+  for (int i = 0; i < 3; ++i) {
+    *(psal + i) = *(psal + i) + *(psal + i) / (i + 1);
+    cout << *(psal + i) << " ";
+  }
+}
+
 // implement the function sortIntegersByPointerArithmetic
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-    string p1("part1");
-    string p2("part2");
+  string p1("part1");
+  string p2("part2");
 
-    if (0 == p1.compare(argv[1]))
-    { // PART I
-        // WRITE THE CODE FOR Part I HERE.
-    }
+  if (0 == p1.compare(argv[1])) { // PART I
+    accessSalaryArrayByPointerArithmetic();
+  }
 
-    if (0 == p2.compare(argv[1]))
-    { // PART II
-        // WRITE THE CODE FOR Part I HERE.
-    }
+  if (0 == p2.compare(argv[1])) { // PART II
+                                  // WRITE THE CODE FOR Part I HERE.
+  }
 
 } // end of main
