e330152616ad0873a027ec7188a5f047132196a6
Mathew Szymanowski <m96szy@gmail.com>
2023-10-02 14:15:36 -0400

    init Lab 4

---
+++ BME506/Lab4Proj/Lab4.cpp
@@ -0,0 +1,8 @@
+#include "Lab4.h"
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+
+} // end of main
---
+++ BME506/Lab4Proj/Lab4.h
@@ -0,0 +1,18 @@
+#ifndef LAB4_H
+#define LAB4_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+enum BloodType { A, AB, O, B };
+enum Organ { Hart, Kidney, Lung, Liver };
+
+struct PatientRecord {};
+
+#endif /* LAB4_H */
