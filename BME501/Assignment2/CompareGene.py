38d0d06c770d4e81462fbe43296bf24d662288d6
Mathew Szymanowski <m96szy@gmail.com>
2023-10-22 21:31:02 -0400

    document and refactor

--- BME501/Assignment2/CompareGene.py
+++ BME501/Assignment2/CompareGene.py
@@ -2,48 +2,6 @@
 # Afrah Chishty 501027994
 # Mya Hao 501108133
 # Mathew Szymanowski 501094808
-# --------------------------------------
-# function to compare
-aminoAcidDic = {'GGG': 'G', 'GGA': 'G', 'GGC': 'G', 'GGT': 'G',
-				'AGG': 'R', 'AGA': 'R', 'AGC': 'S', 'AGT': 'S',
-				'CGG': 'R', 'CGA': 'R', 'CGC': 'R', 'CGT': 'R',
-				'TGG': 'W', 'TGA': 'X', 'TGC': 'C', 'TGT': 'C',
-
-				'GAG': 'E', 'GAA': 'E', 'GAC': 'D', 'GAT': 'D',
-				'AAG': 'K', 'AAA': 'K', 'AAC': 'N', 'AAT': 'N',
-				'CAG': 'Q', 'CAA': 'Q', 'CAC': 'H', 'CAT': 'H',
-				'TAG': 'X', 'TAA': 'X', 'TAC': 'Y', 'TAT': 'Y',
-
-				'GCG': 'A', 'GCA': 'A', 'GCC': 'A', 'GCT': 'A',
-				'ACG': 'T', 'ACA': 'T', 'ACC': 'T', 'ACT': 'T',
-				'CCG': 'P', 'CCA': 'P', 'CCC': 'P', 'CCT': 'P',
-				'TCG': 'S', 'TCA': 'S', 'TCC': 'S', 'TCT': 'S',
-
-				'GTG': 'V', 'GTA': 'V', 'GTC': 'V', 'GTT': 'V',
-				'ATG': 'M', 'ATA': 'I', 'ATC': 'I', 'ATT': 'I',
-				'CTG': 'L', 'CTA': 'L', 'CTC': 'L', 'CTT': 'L',
-				'TTG': 'L', 'TTA': 'L', 'TTC': 'F', 'TTT': 'F',
-				# DNA
-				}
-
-# output missence (M), silent (S) or nonsense (N)
-# will add to count to determine the number of differences
-
-
-def compareCodons(codon1, codon2):
-	# check M -- new amino acid is made
-	if (aminoAcidDic[codon1] != aminoAcidDic[codon2]):
-		return "M"
-	# check S -- same amino acid, different code
-	if (aminoAcidDic[codon1] == aminoAcidDic[codon2]):
-		return "S"
-
-	# check N -- check if it is a stop codon TAA TAG TGA
-	if (aminoAcidDic[codon2] == "TAA" or aminoAcidDic[codon2] == "TAG" or
-			aminoAcidDic[codon2] == "TGA"):
-		return "N"
-# function end
-
 
 # --------------------------------------------------
 # read the file(s)
@@ -52,7 +10,7 @@ data = data.read()
 data = data.replace("\n", "")
 
 # define the dictionary, and location used to find the gene(s)
-# format key: ["NCseq", "ORseq"]
+# format {"key": ["NCseq", "ORseq"]}
 dic = {}
 length = len(data)
 
@@ -87,14 +45,48 @@ while (length > 1):
 	data = data[seqEnd:]
 	length = len(data)
 
-sorted_dict = dict(sorted(dic.items()))
-print(sorted_dict.keys())
+aminoAcidDic = {'GGG': 'G', 'GGA': 'G', 'GGC': 'G', 'GGT': 'G',
+				'AGG': 'R', 'AGA': 'R', 'AGC': 'S', 'AGT': 'S',
+				'CGG': 'R', 'CGA': 'R', 'CGC': 'R', 'CGT': 'R',
+				'TGG': 'W', 'TGA': 'X', 'TGC': 'C', 'TGT': 'C',
 
-# print(len(dic["ORF1a polyprotein NC"]))
-# print(len(dic["ORF1a polyprotein OR"]))
+				'GAG': 'E', 'GAA': 'E', 'GAC': 'D', 'GAT': 'D',
+				'AAG': 'K', 'AAA': 'K', 'AAC': 'N', 'AAT': 'N',
+				'CAG': 'Q', 'CAA': 'Q', 'CAC': 'H', 'CAT': 'H',
+				'TAG': 'X', 'TAA': 'X', 'TAC': 'Y', 'TAT': 'Y',
+
+				'GCG': 'A', 'GCA': 'A', 'GCC': 'A', 'GCT': 'A',
+				'ACG': 'T', 'ACA': 'T', 'ACC': 'T', 'ACT': 'T',
+				'CCG': 'P', 'CCA': 'P', 'CCC': 'P', 'CCT': 'P',
+				'TCG': 'S', 'TCA': 'S', 'TCC': 'S', 'TCT': 'S',
+
+				'GTG': 'V', 'GTA': 'V', 'GTC': 'V', 'GTT': 'V',
+				'ATG': 'M', 'ATA': 'I', 'ATC': 'I', 'ATT': 'I',
+				'CTG': 'L', 'CTA': 'L', 'CTC': 'L', 'CTT': 'L',
+				'TTG': 'L', 'TTA': 'L', 'TTC': 'F', 'TTT': 'F',
+				# DNA
+				}
+
+# output missence (M), silent (S) or nonsense (N)
+# will add to count to determine the number of differences
+def compareCodons(codon1, codon2):
+	# check M -- new amino acid is made
+	if (aminoAcidDic[codon1] != aminoAcidDic[codon2]):
+		return "M"
+	# check S -- same amino acid, different code
+	if (aminoAcidDic[codon1] == aminoAcidDic[codon2]):
+		return "S"
+
+	# check N -- check if it is a stop codon TAA TAG TGA
+	if (aminoAcidDic[codon2] == "TAA" or aminoAcidDic[codon2] == "TAG" or
+			aminoAcidDic[codon2] == "TGA"):
+		return "N"
+# function end
+
 
 # display the table--------------------------------------
 # printing out the table headers
+sorted_dict = dict(sorted(dic.items()))
 print("{:<30} {:<15} {:<10} {:<20} {:<10}"
 	  .format('Gene', 'NC Length', 'OR Length ', 'Difference', 'Dif Count'))
 # printing out the information for the table
