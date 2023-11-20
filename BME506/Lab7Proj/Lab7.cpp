0105867e9dc24dd02a301e615f499f5efb668664
Mathew Szymanowski <m96szy@gmail.com>
2023-11-19 21:35:05 -0500

    Implemented Lab7

--- BME506/Lab7Proj/Lab7.cpp
+++ BME506/Lab7Proj/Lab7.cpp
@@ -1,18 +1,67 @@
 #include "Lab7.h"
 
-Product::Product(int i) { id = i; }
+Product::Product(int i) {
+	// cout << "Product() ctor ..." << endl;
+	id = i;
+}
+
+Product::~Product() {
+	// cout << "~Product() dtor ..." << endl;
+}
 
 int Product::getID() { return id; }
 
 //WRITE THE RELEVANT CODE HERE RELATED TO FACTORY CLASS FOLLOWING 
 //THE SINGLETON PATTERN (STATIC MEMBER INITIALIZATION AND BODIES OF 
 //MEMBER FUNCTIONS).
+Factory* Factory::instance = 0;
 
+Factory::Factory() {
+	// cout << "Factory() ctor ..." << endl;
+}
+
+Factory* Factory::getInstance() {
+	if (instance == 0) {
+		instance = new Factory();
+	}
+	return instance;
+}
+
+void Factory::destroyInstance() {
+	if (instance != 0) {
+		delete instance;
+		instance = 0;
+	}
+}
+
+Product* Factory::createProduct(int id) {
+    Product* newProduct = new Product(id);
+    products.push_back(newProduct);
+	return newProduct;
+}
+
+void Factory::deleteProducts() {
+	int pSize = products.size();
+	for (int i = 0; i < pSize; ++i)
+	{
+		delete products[i];
+		products[i] = 0;
+	}
+}
+
+Factory::~Factory() {
+	deleteProducts();
+	// cout << "~Factory() dtor ..." << endl;
+}
+
+//g++ .\Lab7.cpp -o Lab7.exe
 int main(int argc, char* argv[]) {
 	Factory* f = Factory::getInstance();
 	Product* p1 = f->createProduct(5);
 	Product* p2 = f->createProduct(6);
 	cout << p1->getID() << endl;
 	cout << p2->getID() << endl;
+	
+	Factory::destroyInstance();
 	return 0;
 } //end of main
--- BME506/Lab7Proj/Lab7.h
+++ BME506/Lab7Proj/Lab7.h
@@ -1,6 +1,7 @@
 #ifndef LAB7_H
 #define LAB7_H
 
+#include <deque>
 #include <iostream>
 using namespace std;
 
@@ -9,10 +10,23 @@ private:
 	int id;
 public:
 	Product(int i);
+	~Product();
 	int getID();
 };
 
 //WRITE THE RELEVANT CODE HERE RELATED TO FACTORY CLASS FOLLOWING 
 //THE SINGLETON PATTERN.
+class Factory {
+private:
+	static Factory* instance;
+	deque<Product*> products;
+	void deleteProducts();
+	Factory();
+public:
+	~Factory();
+	static Factory* getInstance();
+	static void destroyInstance();
+	Product* createProduct(int id);
+};
 
 #endif /* LAB7_H */
