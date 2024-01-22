190e43720640a8634abd43ddb0b89acc40f5b36d
Mathew Szymanowski <m96szy@gmail.com>
2024-01-22 12:05:39 -0500

    A3 done

--- BME639/Lab1/Scripts/Part1Script.m
+++ BME639/Lab1/Scripts/Part1Script.m
@@ -1,7 +1,7 @@
-%%A.2
-SignalInput=out.simout(:,1);
-IntegratedSignal=out.simout(:,2);
-DerivativeSignal=out.simout(:,3);
+%% A.2
+SignalInput=out.simoutA1(:,1);
+IntegratedSignal=out.simoutA1(:,2);
+DerivativeSignal=out.simoutA1(:,3);
 
 subplot(3,1,1);
 plot(out.tout, SignalInput);
@@ -24,4 +24,28 @@ xlabel("Time");
 ylabel("Amplitude");
 grid;
 
-%%A.3
+%% A.3
+SignalInput=out.simoutA3(:,1);
+IntegratedSignal=out.simoutA3(:,2);
+DerivativeSignal=out.simoutA3(:,3);
+
+subplot(3,1,1);
+plot(out.tout, SignalInput);
+title("Original Sine Wave Signal");
+xlabel("Time");
+ylabel("Amplitude");
+grid;
+
+subplot(3,1,2);
+plot(out.tout, IntegratedSignal);
+title("Integrated Signal");
+xlabel("Time");
+ylabel("Amplitude");
+grid;
+
+subplot(3,1,3);
+plot(out.tout, DerivativeSignal);
+title("Derived Signal");
+xlabel("Time");
+ylabel("Amplitude");
+grid;
