b39665a72251340b9493d102a821539e12b86251
Tanvir <145484765+Littleboi13@users.noreply.github.com>
2024-01-30 05:23:20 -0500

    upload

--- BME639/Lab1/Scripts/Part1Script.m
+++ BME639/Lab1/Scripts/Part1Script.m
@@ -126,14 +126,20 @@ hold off
 
 figure
 hold on
-plot(out.FOLstep , 'lineWidth', 2.25);
+
+plot(out.time(:,1),out.FOLstep(:,1) , 'lineWidth', 2.25);
+plot(out.time(:,1),out.FOLstep(:,2) , 'lineWidth', 2.25);
+
+
 xlabel('Time (s)');
 ylabel('Amplitude');
 title('Step Function & First Order System', 'FontSize',12);
 legend ('Step Function', 'First Order System');
 grid on;
-ylim([-0.1 1.1]);
+
 dcm = datacursormode;
 dcm.Enable = 'on';
 dcm.DisplayStyle = 'window';
 hold off
+
+
