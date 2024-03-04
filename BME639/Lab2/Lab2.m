d3b7708f797e2ca793b9dd40df21b011a692eef1
Mathew Szymanowski <m96szy@gmail.com>
2024-03-04 12:08:35 -0500

    part2a1 done

--- BME639/Lab2/Lab2.m
+++ BME639/Lab2/Lab2.m
@@ -37,5 +37,16 @@ title ('Voltage-To-Speed with a PI Controller Vs Time')
 legend('Square Wave Input','Voltage to Speed with PI Controller')
 grid;
 hold off
-%% Part 1
+%% Part 2
 %% A.1
+figure
+hold on
+plot(out.time,out.LComp(:,1) , 'Color', '#487F9C', 'lineWidth', 1.25)
+plot(out.time,out.LComp(:,2), 'Color','#DB435E' , 'lineWidth', 1.25)
+
+xlabel('Time (s)')
+ylabel('Amplitude')
+title ('Voltage-To-Speed with a PI Controller Vs Time')
+legend('Square Wave Input','Voltage to Speed with PI Controller')
+grid;
+hold off
