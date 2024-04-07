6479946157ec139a50b569f7557161c6611270d5
Tanvir <145484765+Littleboi13@users.noreply.github.com>
2024-04-07 12:40:36 -0400

    Update Lab3_p2.m

--- BME639/Lab3/Lab3_p2.m
+++ BME639/Lab3/Lab3_p2.m
@@ -1,5 +1,6 @@
 %Part 2
 %% A.1
+clc
 % Variables
 Lp = 0.2; %total length of pendulum
 Mp = 0.097; %pendulum mass
@@ -46,7 +47,7 @@ fprintf('\nMatrix D:\n');
 fprintf('%8.4f\n', D);
 
 %% A.2
-
+clc
 % Characteristic Equation
 charEQ = charpoly(A);
 fprintf('Characteristic Equation of Matrix A is:\n\n');
@@ -58,4 +59,79 @@ eigenvalues = eig(A);
 fprintf('The eigenvalues are:\n\n');
 fprintf('%+.4f\n', eigenvalues);
 
+%% B.1
+clc
 
+% Step 1: Check Controllability of the RIP System
+controlMat = ctrb(A,B);
+fprintf('Controllability Matrix:\n');
+disp(controlMat)
+r = rank(controlMat);
+fprintf('Rank of Controllability Matrix: %d\n\n', r);
+
+% Step 2: Determine Desired Closed-Loop Eigenvalues (Poles) Location
+z = 0.7;
+wn = 4;
+s1 = -z*wn + (wn * sqrt(1-z^2))*1i;
+s2 = -z*wn - (wn * sqrt(1-z^2))*1i;
+fprintf('\n\n\nLocation of the 2 Dominant Poles:\n%.4f + %.4fi\n%.4f + %.4fi\n\n', real(s1), imag(s1), real(s2), imag(s2));
+
+s3 = -30;
+s4 = -40;
+p = [s1 s2 s3 s4];
+desired_char_eq = poly(p);
+fprintf('Desired Closed-Loop Characteristic Equation Coefficients:\n');
+disp(desired_char_eq)
+
+% Step 3: Calculate State-Feedback Gain
+K = place(A, B, p);
+fprintf('\n\n\nGain K is:\n');
+disp(K)
+
+% Step 4: Verify the Closed-Loop Eigenvalues
+Acl = A - B.*K;
+fprintf('\n\n\nClosed-Loop System Matrix (Acl):\n');
+disp(Acl)
+eigenvalues_matrix = eig(Acl);
+fprintf('Eigenvalues of the Closed-Loop System:\n');
+disp(eigenvalues_matrix)
+
+%% B.2  Reference Input and Rotary Arm Angle
+
+figure('Units','normalized','Position',[0.2,0.2,0.6,0.6]); % Larger figure size
+plot(out.time, out.input_Output(:,1), 'LineWidth', 1.5)
+hold on
+plot(out.time, out.input_Output(:,2), 'LineWidth', 1.5)
+hold off
+title('Reference Input and Rotary Arm Angle (\theta)', 'FontSize', 14)
+xlabel('Time (s)', 'FontSize', 12)
+ylabel('\theta (degrees)', 'FontSize', 12)
+legend('Reference Input','Rotary Arm Angle (\theta)', 'FontSize', 10)
+grid on
+ylim([-30 30])
+
+%% Pendulum Angle Deflection
+figure('Units','normalized','Position',[0.2,0.2,0.6,0.6]); % Consistent figure size
+plot(out.time, out.alpha, 'LineWidth', 1.5)
+title('Pendulum Angle Deflection (\alpha)', 'FontSize', 14)
+ylabel('Angular Deflection (degrees)', 'FontSize', 12)
+xlabel('Time (s)', 'FontSize', 12)
+hold on
+[max_value, max_index] = max(abs(out.alpha));
+plot(out.time(max_index), out.alpha(max_index), 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r')
+hold off
+legend('Pendulum Angular Deflection', sprintf('Max Deflection = %.2f degrees', max_value), 'FontSize', 10)
+grid on
+
+%% Control Signal
+figure('Units','normalized','Position',[0.2,0.2,0.6,0.6]); % Making figures consistently large
+plot(out.time, out.control_signal(:,1), 'LineWidth', 1.5)
+title('Control Signal', 'FontSize', 14)
+ylabel('Voltage (V)', 'FontSize', 12)
+xlabel('Time (s)', 'FontSize', 12)
+hold on
+[max_value, max_index] = max(abs(out.control_signal(:,1)));
+plot(out.time(max_index), out.control_signal(max_index,1), 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r')
+hold off
+legend('Control Signal', sprintf('Max Absolute Value = %.2f V', max_value), 'FontSize', 10)
+grid on;
